ccc0504a2feff046eeb51a4bb80c29ad8d962f61
Kimia Darden <kimiadarden@gmail.com>
2020-03-24 23:19:52 -0400

    header function

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return (
+      <div >
+        <h1>Employee Directory</h1>
+        <h6>You can filter your search by name of the employee</h6>
+      </div>
+    )
+  }
+
+export default Header;
