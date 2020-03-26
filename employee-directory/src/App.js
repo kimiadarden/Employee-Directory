0c955b3b1bc57fe1da0f099cfcb28607df5b7f49
Kimia Darden <kimiadarden@gmail.com>
2020-03-25 21:42:05 -0400

    adding json file

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 
 
 function App() {
--- employee-directory/src/components/Header/Header.js
+++ employee-directory/src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./Header.css";
 
 function Header () {
     return (
-      <div >
+      <div className="styleForHeader">
         <h1>Employee Directory</h1>
         <h6>You can filter your search by name of the employee</h6>
       </div>
