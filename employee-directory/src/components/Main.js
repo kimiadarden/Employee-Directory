c36512a704f0f769fb87c13ed0e257bcf2fa0d8e
Kimia Darden <kimiadarden@gmail.com>
2020-03-27 07:56:35 -0400

    adding a search input

--- employee-directory/src/components/Main.js
+++ employee-directory/src/components/Main.js
@@ -1,52 +1,62 @@
-import React, {useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 
-const Main =() =>{
+const Main = () => {
 
-//initilze the state 
+  //initilze the state 
   const [state, setstate] = useState({
 
-    header: [{ name: "Image", width: "20px" },{ name: "name", width: "20px" },{ name: "phone", width: "20px" },{ name: "email", width: "20px" }],
+    header: [{ name: "Image", width: "20px" }, { name: "name", width: "20px" }, { name: "phone", width: "20px" }, { name: "email", width: "20px" }],
     employees: [],
     searchByName: []
   });
 
 
-//grab the user info from the API 
-
-
-useEffect(() => {
-  API.getUsers().then(results => {
-    setstate({
-      ...state,
-      employees: results.data.results,
-      searchByName: results.data.results
+  //grab the user info from the API
+  useEffect(() => {
+    API.getUsers().then(results => {
+      setstate({
+        ...state,
+        employees: results.data.results,
+        searchByName: results.data.results
+      });
     });
-  });
-}, [""]);
+  }, [""]);
 
 
 
-const handlNameSerach = event => {
-  const filter = event.target.value;
-  const filteredList = state.employees.filter(seerchName => {
-  if((seerchName.name.first.toLowerCase()).indexOf(filter.toLowerCase()) !== -1){
-    return seerchName
+  const handlNameSerach = event => {
+    const filter = event.target.value;
+    const filteredList = state.employees.filter(seerchName => {
+      if ((seerchName.name.first.toLowerCase()).indexOf(filter.toLowerCase()) !== -1) {
+        return seerchName
+      };
+    });
+
+    setstate({ ...state, searchByName: filteredList });
   };
-  });
-  
-  setstate({ ...state, searchByName: filteredList });
+
+
+  return (
+
+    <div value={{ state, handlNameSerach }}>
+
+      <nav className="navbar" >
+        <div >
+          <input
+            placeholder="Serach by name"
+            onChange={e => handlNameSerach(e)}
+          />
+        </div>
+      </nav>
+
+
+
+
+
+
+    </div>
+  );
 };
-
-
-
-
-
-
-
-    return (
-        ""
-    );
-  };
 
 export default Main;
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export default {
-    getUsers: function() {
-        
-        // displaying only 20 users 
+  // Gets all users
+  getUsers: function() {
+
+    // return axios.get("https://randomuser.me/api/?results=200&nat=us");
     return axios.get("https://randomuser.me/api/?results=20");
 
   }
