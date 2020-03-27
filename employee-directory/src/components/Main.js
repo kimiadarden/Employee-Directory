add5312489badc4bcaec66ead833dac246f805d5
Kimia Darden <kimiadarden@gmail.com>
2020-03-26 22:54:51 -0400

    initilize the state

--- employee-directory/src/components/Main.js
+++ employee-directory/src/components/Main.js
@@ -1,6 +1,27 @@
 import React from "react";
 
-function Main () {
+const Main =() =>{
+
+
+  const [state, setstate] = useState({
+
+    header: [{ name: "Image", width: "20px" },{ name: "name", width: "20px" },{ name: "phone", width: "20px" },{ name: "email", width: "20px" }],
+    employees: [],
+    searchByName: []
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         ""
     );
