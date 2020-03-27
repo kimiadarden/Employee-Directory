753334147a5ae2cdeb10dd1b548e61277c012ca3
Kimia Darden <kimiadarden@gmail.com>
2020-03-26 22:57:39 -0400

    grab the data from API

--- employee-directory/src/components/Main.js
+++ employee-directory/src/components/Main.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState, useEffect }  from "react";
+import API from "../utils/API";
 
 const Main =() =>{
 
-
+//initilze the state 
   const [state, setstate] = useState({
 
     header: [{ name: "Image", width: "20px" },{ name: "name", width: "20px" },{ name: "phone", width: "20px" },{ name: "email", width: "20px" }],
@@ -11,7 +12,18 @@ const Main =() =>{
   });
 
 
+//grab the user info from the API 
 
+
+useEffect(() => {
+  API.getUsers().then(results => {
+    setstate({
+      ...state,
+      employees: results.data.results,
+      searchByName: results.data.results
+    });
+  });
+}, [""]);
 
 
 
